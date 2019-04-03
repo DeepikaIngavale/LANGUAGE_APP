793301a0cd3e4e42efbcff250ec3ea1ca8c7592e
DeepikaIngavale <deepikaingavale@gmail.com>
2019-04-03 11:15:16 +0530

    Dynamic TextView

--- app/src/main/java/abhinav/com/languageapp/LangHomePage.java
+++ app/src/main/java/abhinav/com/languageapp/LangHomePage.java
@@ -7,25 +7,25 @@ import android.view.View;
 import android.widget.Button;
 
 public class LangHomePage extends AppCompatActivity implements View.OnClickListener {
-    Button btn_login,btn_start;
+    Button btn_speech2Text,btn_start;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_login=(Button)findViewById(R.id.btn_login);
+        btn_speech2Text=(Button)findViewById(R.id.btn_speech2Text);
         btn_start=(Button)findViewById(R.id.btn_start);
-        btn_login.setOnClickListener(this);
+        btn_speech2Text.setOnClickListener(this);
         btn_start.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view)
     {
-        if(view.getId()==R.id.btn_login)
+        if(view.getId()==R.id.btn_speech2Text)
         {
-            Intent intent=new Intent(this,LangLogin.class);
+            Intent intent=new Intent(this,LangSelectAct.class);
             startActivity(intent);
         }
         if(view.getId()==R.id.btn_start)
